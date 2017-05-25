2f92eec7685d94ea50c56e0eaebde58665f72631
AugustoMiotti <a17036@bcc.unifal-mg.edu.br>
2017-05-25 19:51:10 -0300

    fdividi

--- calculadoragithub.c
+++ calculadoragithub.c
@@ -8,12 +8,15 @@ int subtrai(int x, int y){//branch subtrai
 return x-y}
 int multiplica(int x, int y){//multiplica
 return x*y}
-int divide(){}
+int divide(int x, int y){//dividi
+return x/y
+}
 
 int main(){
 	printf("hello git!\n");
 	printf("Soma: %d\n", soma(2,2));
 	printf("Subtração: %d\n", subtrai(2,2));
-	printf("Multiplicação: %d\n", multiplica(2,2)); 
+	printf("Multiplicação: %d\n", multiplica(2,2));
+	printf("Divisão: %d\n", dividi(2,2)); 
 
 }
