a0052030e0a8b08381dfea88d164fa16b3d0b4ee
AugustoMiotti <a17036@bcc.unifal-mg.edu.br>
2017-05-25 19:05:25 -0300

    soma

--- calculadoragithub.c
+++ calculadoragithub.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma(){}
+int soma(int x, int y){
+//alteração feita na branch fsoma return x+y
+}
 int subtrai(){}
 int multiplica(){}
 int divide(){}
 
 int main(){
-	printf("hello git");
+	printf("hello git!\n");
+	printf("Soma: %d\n", soma(2,2)); 
 
 }
